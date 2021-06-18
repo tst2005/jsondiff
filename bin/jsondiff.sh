113f4e3cec678dbbc6a84a165501d6ef18ba8cc4
TsT <tst2005@gmail.com>
2021-06-18 16:08:24 +0200

    manual update, no behavior change

--- bin/jsondiff.sh
+++ bin/jsondiff.sh
@@ -1,36 +1,34 @@
 #!/bin/bash
 
-jsondiff1() {
-
-jq '-S' '-c' 'def json2flat:
-	reduce ( tostream|select(length==2) ) as $i ( {}; .[ $i[0]|map(
-		if type=="number" then
-			"[" + tostring + "]"
-		elif (tostring|test("^[a-zA-Z0-9_]*$")) then
-			"." + tostring
-		else
-			"[" + tojson + "]"
+json_flat_sorted() {
+	jq '-S' '-cM' 'def json2flat:
+		reduce ( tostream|select(length==2) ) as $i ( {}; .[ $i[0]|map(
+			if type=="number" then
+				"[" + tostring + "]"
+			elif (tostring|test("^[a-zA-Z0-9_]*$")) then
+				"." + tostring
+			else
+				"[" + tojson + "]"
+			end
+		) | join("")] = $i[1] )
+	;def hide_last_array_index:
+		to_entries
+		| map(.key|= gsub("\\[([0-9]+)\\]$";"[]"))
+		| map( [.key, .value] )
+		| map_values( {"key": .[0], "value": .[1] } )
+		| map([.]|from_entries)
+	;def walk(f):
+		. as $in
+		| if type == "object" then
+			reduce keys[] as $key
+				( {}; . + { ($key):  ($in[$key] | walk(f)) } ) | f
+		elif type == "array" then map( walk(f) ) | f
+		else f
 		end
-	) | join("")] = $i[1] )
-;def hide_last_array_index:
-	to_entries
-	| map(.key|= gsub("\\[([0-9]+)\\]$";"[]"))
-	| map( [.key, .value] )
-	| map_values( {"key": .[0], "value": .[1] } )
-	| map([.]|from_entries)
-;def walk(f):
-	. as $in
-	| if type == "object" then
-		reduce keys[] as $key
-			( {}; . + { ($key):  ($in[$key] | walk(f)) } ) | f
-	elif type == "array" then map( walk(f) ) | f
-	else f
-	end
-;
-def sortallarrays:
-	walk(if type == "array" and length > 1 then sort else . end)
-;json2flat|hide_last_array_index|sortallarrays|.[]'
-
+	;
+	def sortallarrays:
+		walk(if type == "array" and length > 1 then sort else . end)
+	;json2flat|hide_last_array_index|sortallarrays|.[]'
 }
 
 diff_txt2json() {
@@ -46,8 +44,8 @@ jsondiff() {
 	fi
 	local f1="$1" f2="$2";shift 2
 	diff -u "$@" \
-		<(jsondiff1 < "$f1") \
-		<(jsondiff1 < "$f2") \
+		<(json_flat_sorted < "$f1") \
+		<(json_flat_sorted < "$f2") \
 	| diff_txt2json
 }
 jsondiff "$@"
