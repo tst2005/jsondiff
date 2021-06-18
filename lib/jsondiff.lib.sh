53de78bdf7551218c7177c8c45fc975bdbc90001
TsT <tst2005@gmail.com>
2021-06-18 15:51:34 +0200

    lib use json_flat_sorted

--- lib/jsondiff.lib.sh
+++ lib/jsondiff.lib.sh
@@ -5,17 +5,6 @@ diff_txt2json() {
 	sed -e 's/^\(.\)\(.*\)$/["\1", \2]/g'
 }
 
-#jsondiff() {
-#	local f1="$1" f2="$2";shift 2
-#	jq_run json2flat hide_last_array_index sortallarrays json2ndjson < "$f1"
-#	jq_run json2flat hide_last_array_index sortallarrays json2ndjson < "$f2"
-#}
-
-#jq_run jq_cmd_ json2flat < "$1"
-#jq_run jq_cmd_ json2flat hide_last_array_index < "$1"
-#jq_run jq_cmd_ json2flat hide_last_array_index sortallarrays < "$1"
-#jq_run jq_cmd_ json2flat hide_last_array_index sortallarrays json2ndjson < "$1"
-
 jsondiff() {
 	if [ $# -lt 2 ]; then
 		echo >&2 "Missing argument"
@@ -23,8 +12,7 @@ jsondiff() {
 	fi
 	local f1="$1" f2="$2";shift 2
 	diff -u "$@" \
-		<(jq_run json2flat hide_last_array_index sortallarrays json2ndjson < "$f1") \
-		<(jq_run json2flat hide_last_array_index sortallarrays json2ndjson < "$f2") \
+		<(json_flat_sorted < "$f1") \
+		<(json_flat_sorted < "$f2") \
 	| diff_txt2json
 }
-
